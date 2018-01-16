d699a882b4b0e8edb49c4ce0466389905d67ca8f
Protyasha-Saha <sahaprotyasha@gmail.com>
2018-01-16 11:16:06 +0600

    modified

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Kyoto Engineering & Automation</title>
+  <script language="javascript">
+    window.location.href = "package/vHome/us/products/pages/BaginBox_packaging.php"
+</script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="assets/css/style_homepage.css" rel="stylesheet" type="text/css"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+     <img id="bg" src="assets/images/landing Banner.jpg" allign="middle"   alt="2017"> 
+</body>
+</html>
